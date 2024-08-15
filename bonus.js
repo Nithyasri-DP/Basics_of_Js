e5f786ade4d39b671550dbf70c3959232f8d1da1
Nithyasri-DP <dpnithyasri75@gmail.com>
2024-08-15 23:03:59 +0530

    added file

---
+++ bonus.js
@@ -0,0 +1,7 @@
+function bonus(salary) {
+    return salary * 0.2;
+}
+let totbonus;
+salary=50000;
+totbonus=bonus(salary);
+console.log("Bonus amount is:",totbonus);
---
+++ circlearea.js
@@ -0,0 +1,8 @@
+let pi = 3.14;
+    function fun(rad) {
+        return (pi * rad * rad);
+    }
+    let rad, Area;
+    rad = 15;
+    Area = fun(rad);
+    console.log("Area of Circle is: ", Area);
